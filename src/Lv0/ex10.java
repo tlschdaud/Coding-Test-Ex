7949ba6a6370f7317644d4c1228f4921655a0b69
Shin ChongMyeong <tlschdaud1@naver.com>
2023-11-24 17:14:25 +0900

    1124

--- src/Lv0/ex10.java
+++ src/Lv0/ex10.java
@@ -1,4 +1,17 @@
 package Lv0;
 
 public class ex10 {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s) + overwrite_string;
+        if(my_string.length() > answer.length()) {
+            answer += my_string.substring(answer.length());
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex10 ex10 = new ex10();
+        System.out.println(ex10.solution("He11oWor1dddddd",	"lloWorl",	2));
+
+    }
 }
