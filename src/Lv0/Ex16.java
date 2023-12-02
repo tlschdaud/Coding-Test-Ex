7f02062d22d8d0315c18dc66d54b40dd98c851ea
Shin ChongMyeong <tlschdaud1@naver.com>
2023-12-02 11:31:48 +0900

    1202

--- src/Lv0/Ex16.java
+++ src/Lv0/Ex16.java
@@ -1,5 +1,7 @@
 package Lv0;
 
+// n의 배수
+//정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
 public class Ex16 {
     public int solution(int num, int n) {
 
